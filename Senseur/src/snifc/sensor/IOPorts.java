6687ffe71db7d4e562d4f0505cd0617886ca1b8c
mat.declercq <mat.declercq@0d88164a-b74d-0410-813e-8b896406d51d>
2008-05-21 09:55:47 +0000

    ajout de la classe IOPorts

--- Senseur/src/snifc/sensor/IOPorts.java
+++ Senseur/src/snifc/sensor/IOPorts.java
@@ -8,6 +8,7 @@ package snifc.sensor;
 import snifc.LinkIfc;
 import snifc.PacketIfc;
 import java.util.Vector;
+import snifc.Link;
 import snifc.Simulator;
 /**
  *
@@ -20,30 +21,45 @@ public class IOPorts implements IOPortsIfc{
 private int id;
 private Vector lVector;
 private Queue queueIOPort;
+private int linkIdWriter;
 
     public void IOPorts(int id, Queue queueIOPort){
         
         this.id = id;
         this.lVector = new Vector();
         this.queueIOPort = queueIOPort;
-        
+        this.linkIdWriter = 0;
     }
 
     public void addLink(LinkIfc l) throws Exception {
-        
+        System.out.println("On ajoute un lien au port");
         this.lVector.add(l);
-        
-        
+  
     }
 
     public void writePacket(PacketIfc p) {
         
-        
-        throw new UnsupportedOperationException("Not supported yet.");
+       System.out.println("On ecrit le paquet dans un lien");
+        for(int i=0; i< this.lVector.size(); i++){
+            ((Link)this.lVector.get(i)).transmit(p, this);
+        }
+  
     }
 
-    public PacketIfc getPackets() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void getPackets() {
+        
+        System.out.println("On fait un get packet");
+        for(int i=0; i< this.lVector.size(); i++){
+
+          if (this.queueIOPort.isFull()){
+        }
+        else{
+        this.queueIOPort.enQueue(((Link)this.lVector.get(i)).getPendingPacket(this));
+                
+        }
+          
+        }
+        
     }
 
 }
